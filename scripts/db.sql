5fab5398b372d1f96c67074973b6dea0edbdf5b8
Carlos Eduardo <ferreiira.kadu@gmail.com>
2022-11-12 14:13:20 -0300

    refactor: db script

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,16 +1,20 @@
+create database eleicao;
+
+use eleicao;
+
 create table if not exists eleicao
 (
   id         bigint unsigned auto_increment
-  primary key,
+    primary key,
   ativa      tinyint(1) default 1 not null,
   created_at timestamp            null,
   updated_at timestamp            null
-  );
+);
 
 create table if not exists chapa
 (
   id              bigint unsigned auto_increment
-  primary key,
+    primary key,
   nome_chapa      varchar(255)    not null,
   cod_chapa       varchar(255)    not null,
   nome_sindico    varchar(255)    not null,
@@ -21,14 +25,14 @@ create table if not exists chapa
   created_at      timestamp       null,
   updated_at      timestamp       null,
   constraint chapa_cod_votacao_foreign
-  foreign key (cod_eleicao) references eleicao (id)
-  );
+    foreign key (cod_eleicao) references eleicao (id)
+);
 
 
 create table if not exists voto
 (
   id          bigint unsigned auto_increment
-  primary key,
+    primary key,
   bloco       varchar(255)    not null,
   apartamento varchar(255)    not null,
   cod_chapa   bigint unsigned not null,
@@ -36,7 +40,9 @@ create table if not exists voto
   created_at  timestamp       null,
   updated_at  timestamp       null,
   constraint voto_cod_chapa_foreign
-  foreign key (cod_chapa) references chapa (id),
+    foreign key (cod_chapa) references chapa (id),
   constraint voto_cod_eleicao_foreign
-  foreign key (cod_eleicao) references eleicao (id)
-  );
+    foreign key (cod_eleicao) references eleicao (id)
+);
+
+
